707e4859e4aa0dc9796e93bdaaa8462bcfc3f39b
Nileena <nileena.ps123@gmail.com>
2026-03-07 22:15:53 +0530

    Component Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,11 @@
-  import './App.css';
-  import {Status} from './components/Status'
-  import {Heading} from './components/Heading'
-  import {Oscar} from './components/Oscar'
-  import {Greet} from './components/Greet'
-  import {Button} from './components/Button'
-  import {Input} from './components/Input'
-  import {Container} from './components/Container'
-  import {PersonList} from './components/PersonList'
- 
-  import {ThemeContextProvider} from './components/context/ThemeContext'
-  import {Box} from './components/context/Box'
-  import {User} from './components/context/User'
-  import {UserContext, UserContextProvider} from './components/context/UserContext'
-
+import './App.css';
+import {List} from  './components/generics/List'  
 function App() {
-    const nameList = [
-   {name: {
-      fname: 'Bruce',
-      lname: 'Wayne'
-    }},
-    {name:{
-      fname: 'Clark',
-      lname: 'Kent'
-    }},
-    {name:{
-      fname: 'Princess',
-      lname: 'Diana'
-    }}
-  ]
-
+    
   return (
     <div className="App">
-{/*       <Status status='loading'/>
-      <PersonList names={nameList} />
-      <Heading>Place holder text</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Decaprio</Heading>
-      </Oscar>
-      <Greet name="unni" isLoggedIn={false}></Greet>
-      <Button handleClick={(event,id)=>{
-        console.log("Button Clicked",event,id)
-        }}
-        />
-      <Input value='' handleChange={e=>console.log(e)}></Input>  
-      <Container styles={{border:'1px solid black',padding:'lrem'}}></Container> */}
-
-{/*        <ThemeContextProvider>
-        <Box/>
-       </ThemeContextProvider> */}
-
-       <UserContextProvider>
-        <User/>
-       </UserContextProvider>
- 
+      <List items={['Batman','wonderwoman','super man']}
+      onClick={(item)=>console.log(item)}></List>
     </div>
   );
 }
